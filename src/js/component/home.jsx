e5cea667d7be41e50d798970517f262d9fdc6f05
Mariona Estrada <165200714+estradapm@users.noreply.github.com>
2024-06-18 17:45:48 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,19 +6,36 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex justify-content-between bg-body-tertiary" style={{ backgroundColor: 'black', color: 'white' }}>
+			<nav className="navbar">
+				<div className="container-fluid">
+					<a className="navbar-brand" href="#" style={{ color: 'white' }}>
+						<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/640px-Bootstrap_logo.svg.png" alt="Logo" width="30" height="24" className="d-inline-block align-text-top"/>
+						Start Bootstrap
+					</a>
+				</div>
+			</nav>
+
+			<nav className="navbar navbar-expand-lg" style={{ color: 'white' }}>
+				<div className="container-fluid">
+					<div className="collapse navbar-collapse" id="navbarNav">
+						<ul className="navbar-nav">
+							<li className="nav-item">
+								<a className="nav-link active" aria-current="page" href="#" style={{ color: 'white' }}>Home</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#" style={{ color: 'white' }}>About</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#" style={{ color: 'white' }}>Services</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#" style={{ color: 'white' }}>Contact</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
 		</div>
 	);
 };
